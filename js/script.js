281473e73ac72879b1b0dd6f1ca05ac2e5f30494
Letenyei Ádám <letenyeidam89@gmail.com>
2021-05-02 19:18:04 +0200

    Dropdown
    Menu created

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 const menuBtn = document.querySelector('.menu-btn');
+const dropDown = document.querySelector('.menu_list');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
     if(!menuOpen) {
         menuBtn.classList.add('open');
         menuOpen = true;
+        dropDown.style.display = 'block';
+        dropDown.style.margin = '50px 200px 100px -75px';
     } else {
         menuBtn.classList.remove('open');
         menuOpen = false;
+        dropDown.style.display = 'none';
     }
 });
