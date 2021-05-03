e134a451577ab126bfaee4d676dafad3d3389710
Letenyei Ádám <letenyeidam89@gmail.com>
2021-05-03 21:23:40 +0200

    Js
    Scroll to function implemented

--- js/script.js
+++ js/script.js
@@ -13,3 +13,26 @@ menuBtn.addEventListener('click', () => {
         dropDown.style.display = 'none';
     }
 });
+
+//const churches = document.getElementById('churches');
+
+function scrollToChurches(btnId) {
+    let element;
+    let elementSelector;
+    
+    if(btnId == "churchesBtn") {
+        element = 'churches';
+    } else if(btnId == "eventsBtn") {
+        element = 'events_section';
+    } else if(btnId == "newsBtn") {
+        element = 'news';
+    }
+    elementSelector = document.getElementById(element);
+
+   const yOffset = -102;
+   const y = elementSelector.getBoundingClientRect().top + window.pageYOffset + yOffset;
+   window.scrollTo({
+       top: y,
+       behavior: 'smooth'
+   });
+}
